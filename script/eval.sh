2bb07e2d86f2e813d96482977fb1bd910386608d
FumiyukiKato <fumilemon79@gmail.com>
2021-04-06 14:19:26 +0900

    update sensitive information

--- script/eval.sh
+++ script/eval.sh
@@ -8,10 +8,11 @@ mech=$4
 cate_num=$5
 epsilon=$6
 times=$7
+g=$8
 
 for i in $(seq 1 $times) ; do
     echo $i
-    python server.py --mech $mech --cate_num $cate_num --width $width --epsilon $epsilon --port $port --address $address &
+    python server.py --mech $mech --cate_num $cate_num --width $width --epsilon $epsilon --port $port --address $address --g $g &
     sleep 0.1
-    python client.py --mech $mech --cate_num $cate_num --width $width --epsilon $epsilon --port $port --address $address --sensitive_value 2
+    python client.py --mech $mech --cate_num $cate_num --width $width --epsilon $epsilon --port $port --address $address --sensitive_value 1 --g $g
 done
