f45634832aab723c928277a8af64a540636aab03
FumiyukiKato <fumilemon79@gmail.com>
2021-04-07 09:10:01 +0900

    automate experiment

--- script/eval.sh
+++ script/eval.sh
@@ -1,18 +1,26 @@
 #! /bin/bash -x
 # usage
-#  - script/eval.sh 50006 127.0.0.1 100 krr 10 1.0 10 10
+#  - script/eval.sh 50006 127.0.0.1 32 1.0 10
 port=$1
 address=$2
-width=$3
-mech=$4
-cate_num=$5
-epsilon=$6
-g=$7
-times=$8
+cate_max=$3
+epsilon=$4
+times=$5
 
-for i in $(seq 1 $times) ; do
-    echo $i
-    python server.py --mech $mech --cate_num $cate_num --width $width --epsilon $epsilon --port $port --address $address --g $g &
-    sleep 0.1
-    python client.py --mech $mech --cate_num $cate_num --width $width --epsilon $epsilon --port $port --address $address --sensitive_value 1 --g $g
+g=0
+for width in 100 1000; do
+	for mech in krr oue olh; do
+		for cate_num in $(seq 2 $cate_max); do
+			for i in $(seq 1 $times) ; do
+				if [ $cate_num -le 3 ] ; then
+					g=2
+				else
+					g=$(($cate_num / 2))
+				fi
+				python server.py --mech $mech --cate_num $cate_num --width $width --epsilon $epsilon --port $port --address $address --g $g &
+				sleep 1
+				python client.py --mech $mech --cate_num $cate_num --width $width --epsilon $epsilon --port $port --address $address --sensitive_value 1 --g $g
+			done
+		done
+	done
 done
